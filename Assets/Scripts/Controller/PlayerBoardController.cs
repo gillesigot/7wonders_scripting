75093c682358d7de7fb094d55238aafecbae99a5
Gilles Igot <gilles.igot@gmail.com>
2020-05-30 22:40:31 +0200

    Add war score

--- Assets/Scripts/Controller/PlayerBoardController.cs
+++ Assets/Scripts/Controller/PlayerBoardController.cs
@@ -7,18 +7,29 @@ public class PlayerBoardController
     public static Player Player { get; set; }
     // Used to store the UI component that display the coins amount.
     public static Text Coins { get; set; }
-    // TODO war_text
+    // Used to store the UI component that display the war points.
+    public static Text WarPoints { get; set; }
 
     public PlayerBoardController()
     {
         Player = GameManager.Instance().getHumanPlayer();
         Coins = GameObject.Find("coin_text").GetComponent<Text>();
+        WarPoints = GameObject.Find("war_text").GetComponent<Text>();
     }
+
     /// <summary>
     /// Refresh the coin amount on the player board.
     /// </summary>
     public static void RefreshCoinAmount()
     {
         Coins.text = Player.Coins.ToString();
+    }
+
+    /// <summary>
+    /// Refresh the war points count on the player board.
+    /// </summary>
+    public static void RefreshWarPoints()
+    {
+        WarPoints.text = Player.VictoryWarPoints.ToString();
     }
 }
